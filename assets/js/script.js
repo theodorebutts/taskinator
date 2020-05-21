04d4afae5d8870da02362e24a526e363ec1785b4
Theodore Butts <theodorebutts@gmail.com>
2020-05-20 20:07:41 -0600

    form submit initial code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,35 @@
+var formEl = document.querySelector('#task-form');
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-var createTaskHandler = function () {
+
+var taskFormHandler = function () {
+
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+        var taskDataObj = {
+            name: taskNameInput,
+            type: taskTypeInput
+        };
+
+        createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function (taskDataObj) {
+    name: "Task's name";
+    type: "Task's type"
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    
+    listItemEl.appendChild(taskInfoEl);
+    
     tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", createTaskHandler);
-
+formEl.addEventListener('submit', taskFormHandler);
